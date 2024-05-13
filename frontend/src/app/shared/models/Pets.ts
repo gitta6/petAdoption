3c4184ccf210d562a4acdf4af455b2f294a12fa8
Gitta Potoczki <gitta.potoczki@gmail.com>
2024-05-13 19:01:37 +0200

    added header, pets model, instances,...

---
+++ frontend/src/app/shared/models/Pets.ts
@@ -0,0 +1,12 @@
+export class Pets{
+    id!:string;
+    name!:string;
+    age!:number;
+    species!:string;
+    breed!:string;
+    gender!:string;
+    imageUrl!:string;
+    color?:string;
+    description?:string;
+    location?: string;
+}
---
+++ frontend/src/data.ts
@@ -0,0 +1,133 @@
+import { Pets } from './app/shared/models/Pets';
+
+export const sample_pets: Pets[] = [
+    {
+        id: '1',
+        name: 'Mirci',
+        age: 3,
+        species: 'macska',
+        breed: 'házimacska',
+        gender: 'nőstény',
+        imageUrl: 'assets/pet_id1.jpg',
+        color: 'trikolor',
+        description: 'szelíd, bújós, játékos',
+        location: 'Egerszalók'
+    },
+
+    {
+        id: '2',
+        name: 'Pillecukor',
+        age: 2,
+        species: 'kutya',
+        breed: 'mopsz',
+        gender: 'hím',
+        imageUrl: 'assets/pet_id2.jpg',
+        color: 'őzbarna',
+        description: 'energikus, barátságos, kedves',
+        location: 'Budapest'
+    },
+
+    {
+        id: '3',
+        name: 'Pufi',
+        age: 1,
+        species: 'macska',
+        breed: 'perzsa',
+        gender: 'hím',
+        imageUrl: 'assets/pet_id3.jpg',
+        color: 'fehér',
+        description: 'játékos, csendes, hűséges',
+        location: 'Debrecen'
+    },
+
+    {
+        id: '4',
+        name: 'Foltos',
+        age: 4,
+        species: 'kutya',
+        breed: 'foxterrier',
+        gender: 'hím',
+        imageUrl: 'assets/pet_id4.jpg',
+        color: 'fekete-fehér',
+        description: 'vidám, intelligens, odaadó',
+        location: 'Szeged'
+    },
+
+    {
+        id: '5',
+        name: 'Sanyi',
+        age: 2,
+        species: 'macska',
+        breed: 'bengáli',
+        gender: 'nőstény',
+        imageUrl: 'assets/pet_id5.jpg',
+        color: 'barna-fekete',
+        description: 'játékos, kíváncsi, édes',
+        location: 'Győr'
+    },
+
+    {
+        id: '6',
+        name: 'Rexi',
+        age: 5,
+        species: 'kutya',
+        breed: 'labrador',
+        gender: 'kan',
+        imageUrl: 'assets/pet_id6.jpg',
+        color: 'barna',
+        description: 'barátságos, derűs, gyengéd',
+        location: 'Pécs'
+    },
+    
+    {
+        id: '7',
+        name: 'Zizi',
+        age: 3,
+        species: 'macska',
+        breed: 'egyiptomi mau',
+        gender: 'nőstény',
+        imageUrl: 'assets/pet_id7.jpg',
+        color: 'szürke',
+        description: 'elegáns, hűséges, nyugodt',
+        location: 'Szombathely'
+    },
+
+    {
+        id: '8',
+        name: 'Bubu',
+        age: 1,
+        species: 'kutya',
+        breed: 'puli',
+        gender: 'nőstény',
+        imageUrl: 'assets/pet_id8.jpg',
+        color: 'fekete',
+        description: 'játékos, éber, bátor',
+        location: 'Miskolc'
+    },
+
+    {
+        id: '9',
+        name: 'Tappancs Professzor',
+        age: 2,
+        species: 'macska',
+        breed: 'maine coon',
+        gender: 'hím',
+        imageUrl: 'assets/pet_id9.jpg',
+        color: 'vörös',
+        description: 'impozáns, barátságos, okos',
+        location: 'Székesfehérvár'
+    },
+    
+    {
+        id: '10',
+        name: 'Megatron',
+        age: 1,
+        species: 'kutya',
+        breed: 'golden retriever',
+        gender: 'hím',
+        imageUrl: 'assets/pet_id10.jpg',
+        color: 'arany',
+        description: 'vidám, szeretetteljes, lelkes',
+        location: 'Kecskemét'
+    }    
+]
