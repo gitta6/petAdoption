38ed90600806c43a9dd352ab2b89f4c8d3176c3b
Gitta Potoczki <gitta.potoczki@gmail.com>
2024-06-17 23:48:22 +0200

    added a few default components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,9 +15,16 @@ import { LoginPageComponent } from './components/pages/login-page/login-page.com
 import { ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
+import { DefaultButtonComponent } from './components/partials/default-button/default-button.component';
+import { InputContainerComponent } from './components/partials/input-container/input-container.component';
+import { InputValidationComponent } from './components/partials/input-validation-component/input-validation.component';
+import { TextInputComponent } from './components/partials/text-input/text-input.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, CategoriesComponent, PetPageComponent, FavoritesPageComponent, TitleComponent, NotFoundComponent, LoginPageComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, CategoriesComponent,
+    PetPageComponent, FavoritesPageComponent, TitleComponent, NotFoundComponent, LoginPageComponent,
+    RegisterPageComponent, DefaultButtonComponent, InputContainerComponent, InputValidationComponent, TextInputComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule,
     HttpClientModule, ReactiveFormsModule,
     ToastrModule.forRoot({ timeOut: 3000, positionClass: 'toast-bottom-right', newestOnTop: false })],
