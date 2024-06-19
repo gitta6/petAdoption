227da94c4d1206e2db4ab964af04f4097d6618fc
Gitta Potoczki <gitta.potoczki@gmail.com>
2024-06-19 21:11:47 +0200

    added loading

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './components/partials/header/header.component';
 import { SearchComponent } from './components/partials/search/search.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { CategoriesComponent } from './components/partials/categories/categories.component';
-import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { PetPageComponent } from './components/pages/pet-page/pet-page.component';
 import { FavoritesPageComponent } from './components/pages/favorites-page/favorites-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
@@ -20,15 +20,21 @@ import { DefaultButtonComponent } from './components/partials/default-button/def
 import { InputContainerComponent } from './components/partials/input-container/input-container.component';
 import { InputValidationComponent } from './components/partials/input-validation-component/input-validation.component';
 import { TextInputComponent } from './components/partials/text-input/text-input.component';
+import { LoadingComponent } from './components/partials/loading/loading.component';
+import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, CategoriesComponent,
     PetPageComponent, FavoritesPageComponent, TitleComponent, NotFoundComponent, LoginPageComponent,
-    RegisterPageComponent, DefaultButtonComponent, InputContainerComponent, InputValidationComponent, TextInputComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule,
-    HttpClientModule, ReactiveFormsModule,
-    ToastrModule.forRoot({ timeOut: 3000, positionClass: 'toast-bottom-right', newestOnTop: false })],
-  providers: [provideHttpClient(withFetch())],
+    RegisterPageComponent, DefaultButtonComponent, InputContainerComponent, InputValidationComponent, TextInputComponent,
+    LoadingComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, HttpClientModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot({ timeOut: 3000, positionClass: 'toast-bottom-right', newestOnTop: false })
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
